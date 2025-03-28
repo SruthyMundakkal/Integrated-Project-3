e348af768062ad55633486a9e1e7353bef4e793f
JR <juniordee3@gmail.com>
2025-03-28 15:05:46 -0300

    update dependencies
    update package-lock
    
    update package-lock
    
    make required changes to implement tailwind v4
    
    Merge branch 'feature/update-dependencies'
    
    update package-lock
    
    make necessary changes to build app with nextjs v14 and tailwind v4
    
    remove local dev files as we migrated to the cloud
    
    update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,5 @@
 // import DeployButton from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import HeaderAuth from "@/components/header-auth";
 import { ThemeSwitcher } from "@/components/theme-switcher";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
-import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
 import "./globals.css";
@@ -18,18 +14,13 @@ export const metadata = {
   description: "The fastest way to build apps with Next.js and Supabase",
 };
 
-const geistSans = Geist({
-  display: "swap",
-  subsets: ["latin"],
-});
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={geistSans.className} suppressHydrationWarning>
+    <html lang="en">
       <body className="bg-background text-foreground">
         <ThemeProvider
           attribute="class"
@@ -47,7 +38,6 @@ export default function RootLayout({
                       <DeployButton />
                     </div> */}
                   </div>
-                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
               </nav>
               <div className="flex flex-col gap-20 max-w-7xl p-5">
